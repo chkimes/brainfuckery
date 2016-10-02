2808a7ecd8aac7f224fc75115328bd72a2f07396
Chad Kimes <ckimes89@gmail.com>
2016-10-02 17:11:06 -0400

    Put built-ins into their own file

---
+++ Brainfuckery/BuiltIns.fs
@@ -0,0 +1,19 @@
+﻿module BuiltIns
+open CompileBf
+open System.Collections.Generic
+
+let compiledAdd =
+    { Name = "add"; Variables = new Dictionary<string, int>() ; Parameters = [ "a"; "b" ] ; Code = ">[-<+>]<" }
+
+let compiledRead =
+    { Name = "read"; Variables = new Dictionary<string, int>() ; Parameters = [] ; Code = "," }
+
+let compiledWrite =
+    { Name = "write"; Variables = new Dictionary<string, int>() ; Parameters = [ "a" ] ; Code = "." }
+
+let builtIns =
+    seq {
+        yield compiledAdd
+        yield compiledRead
+        yield compiledWrite
+    } |> Seq.map Compiled |> List.ofSeq
--- Brainfuckery/Program.fs
+++ Brainfuckery/Program.fs
@@ -2,23 +2,8 @@
 open HighLevel
 open LowLevel
 open CompileBf
+open BuiltIns
 open System.Collections.Generic
-
-let compiledAdd =
-    { Name = "add"; Variables = new Dictionary<string, int>() ; Parameters = [ "a"; "b" ] ; Code = ">[-<+>]<" }
-
-let compiledRead =
-    { Name = "read"; Variables = new Dictionary<string, int>() ; Parameters = [] ; Code = "," }
-
-let compiledWrite =
-    { Name = "write"; Variables = new Dictionary<string, int>() ; Parameters = [ "a" ] ; Code = "." }
-
-let builtIns =
-    seq {
-        yield compiledAdd
-        yield compiledRead
-        yield compiledWrite
-    } |> Seq.map Compiled |> List.ofSeq
 
 [<EntryPoint>]
 let main argv = 
