36361577e6c7a29cb861e34fe08bd757ca3587dc
Chad Kimes <ckimes89@gmail.com>
2016-10-02 17:08:37 -0400

    Add built-in read/write methods

--- Brainfuckery/Program.fs
+++ Brainfuckery/Program.fs
@@ -7,9 +7,17 @@ open System.Collections.Generic
 let compiledAdd =
     { Name = "add"; Variables = new Dictionary<string, int>() ; Parameters = [ "a"; "b" ] ; Code = ">[-<+>]<" }
 
+let compiledRead =
+    { Name = "read"; Variables = new Dictionary<string, int>() ; Parameters = [] ; Code = "," }
+
+let compiledWrite =
+    { Name = "write"; Variables = new Dictionary<string, int>() ; Parameters = [ "a" ] ; Code = "." }
+
 let builtIns =
     seq {
         yield compiledAdd
+        yield compiledRead
+        yield compiledWrite
     } |> Seq.map Compiled |> List.ofSeq
 
 [<EntryPoint>]
